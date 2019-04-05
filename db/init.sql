008d7a86c920265f68f22b9d45822410f5a287a3
Lee Odell <leerodell@gmail.com>
2019-04-05 11:02:21 -0700

    setup complete

---
+++ db/init.sql
@@ -0,0 +1,11 @@
+initial data
+
+create table products (
+product_id serial primary key,
+product_name text not null,
+product_type text not null,
+quantity integer not null
+);
+
+insert into products (product_name, product_type, quantity)
+values ('teddy', 'toy', 5), ('wrench', 'tool', 100), ('galaxy', 'phone', 1);
--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require ('express');
-const bodyParser = require ('body-parser');
-
-const ctr = require('./controller')
-
 const app = express();
-
+const bodyParser = require ('body-parser');
+const massive = require ('massive')
+const ctr = require('./controller')
+require("dotenv").config();
 app.use(bodyParser.json());
+
+massive(process.env.CONNECTION_STRING).then(db => {
+    app.set('db', db)
+    db.init()
+    console.log('connected to db')
+})
+
+
 
 const PORT = 3001;
 
